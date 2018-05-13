56fe046eaabf3063ad3fbfe7231fd4575a7b74e6
Somenath Dey <somnath2prl@gmail.com>
2018-05-13 19:11:51 +0530

    Fix datasource.

--- server/models/rfi.js
+++ server/models/rfi.js
@@ -3,9 +3,18 @@
 module.exports = function(Rfi) {
     Rfi.listRfi = function(title, callback) {
         console.log(title);
+        console.log(Rfi.customTitle());
         var DataAPI = Rfi.app.datasources.DataAPI;
         return callback(null, {'Title': title});
         //console.log(DataAPI.accessToken);
+    }
+    Rfi.remoteMethod('listRfi', {
+        accepts: {arg: 'title', type: 'string'},
+        returns: {arg: 'data', type: 'object'},
+    });
+    
+    Rfi.customTitle = function() {
+        //console.log(this);
     }
 
 };
